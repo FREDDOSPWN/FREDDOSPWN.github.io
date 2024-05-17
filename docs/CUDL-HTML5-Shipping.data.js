6a41c067fd46ec3f8cb63fe039c6f14c49bd51c7
Nathan Goodwin <goodwinnj@cardiff.ac.uk>
2024-05-17 15:53:38 +0100

    Login changes
    As last commit, but for the docs folder.

--- docs/CUDL-HTML5-Shipping.data.js
+++ docs/CUDL-HTML5-Shipping.data.js
@@ -173,6 +173,6 @@ Module['FS_createPath']('/CUDL/Content', 'Paks', true, true);
   }
 
  }
- loadPackage({"files": [{"start": 0, "audio": 0, "end": 53, "filename": "/Manifest_NonUFSFiles_HTML5.txt"}, {"start": 53, "audio": 0, "end": 81, "filename": "/UE4CommandLine.txt"}, {"start": 81, "audio": 0, "end": 60116643, "filename": "/CUDL/Content/Paks/CUDL-HTML5.pak"}], "remote_package_size": 60116643, "package_uuid": "5090c16d-f79e-46bd-b98b-2594287da0c3"});
+ loadPackage({"files": [{"start": 0, "audio": 0, "end": 53, "filename": "/Manifest_NonUFSFiles_HTML5.txt"}, {"start": 53, "audio": 0, "end": 81, "filename": "/UE4CommandLine.txt"}, {"start": 81, "audio": 0, "end": 60120065, "filename": "/CUDL/Content/Paks/CUDL-HTML5.pak"}], "remote_package_size": 60120065, "package_uuid": "62985ef9-08a1-4b51-ae36-e4e9ad728adf"});
 
 })();
